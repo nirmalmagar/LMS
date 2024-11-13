417ac1e2e8b8032772478b060a1a072cc2885853
Nirmal Magar <nirmalmagar.5599@gmail.com>
2024-11-13 11:28:56 +0545

    create  book info component

--- app/book-list/[book_id]/page.tsx
+++ app/book-list/[book_id]/page.tsx
@@ -5,6 +5,7 @@ import useSWR from "swr";
 import Image from "next/image";
 import { defaultFetcher } from "@/helpers/FetchHelper";
 import Container from "@/components/Container";
+import OtherInfo from "@/components/Elements/OtherInfo/OtherInfo";
 import HomeLayout from "@/components/Layouts/HomeNavBar/HomeLayout";
 
 const page = () => {
@@ -31,8 +32,16 @@ const page = () => {
             <div className="col-span-2">
               <span className="bg-gray-300 rounded-md px-2 py-0.5">Paper Back</span>
               <h1>{bookId?.title}</h1>
-              <span>by: {bookId?.auther}</span>
-              
+              <span>by: {bookId?.author}</span>
+              <h2>Sold By: Gyan Kosh Nepal</h2>
+              <div>
+                <h3>Synopsis</h3>
+                <p>{bookId?.description}</p>
+              </div>
+              <div>
+                <p>Other Info</p>
+                <OtherInfo/>
+              </div>
             </div>
             <div></div>
           </div>
---
+++ components/Elements/OtherInfo/OtherInfo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Image from "next/image";
+import { MdOutlineLibraryBooks } from "react-icons/md";
+
+const OtherInfo = () => {
+  return <div>
+    <h5>Page Count</h5>
+    <div className="relative w-40 h-40">
+    <Image fill src={} alt="logo"/>
+    </div>
+    <p>304 Pages</p>
+
+  </div>;
+};
+
+export default OtherInfo;
