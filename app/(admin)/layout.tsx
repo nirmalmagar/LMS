13388cdcd51b39d8f9a929e083ad4043a2a6215c
Nirmal Magar <nirmalmagar.5599@gmail.com>
2025-01-25 22:02:51 +0545

    change on admin layout so, login process will be redirect fast

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,30 +1,37 @@
 "use client";
-import { ReactNode, Suspense, useEffect } from "react";
+import { ReactNode, Suspense, useEffect, useState } from "react";
 import { routes } from "@/utils/routes";
 import Cookies from "js-cookie";
 import { AppColorThemeProvider } from "@/context/ColorTheme";
 import { useRouter } from "next/navigation";
-import {AuthProvider} from "@/context/AuthContext";
+import { AuthProvider } from "@/context/AuthContext";
+import Loader from "@/components/common/Loader";
 
 const AdminRootLayout = ({ children }: { children: ReactNode }) => {
   const router = useRouter();
   const token = Cookies.get("LOGIN_TOKEN");
 
+  const [loading, setLoading] = useState<boolean>(true);
   useEffect(() => {
-    if (token) {
-      router.push(routes.ADMIN_DASHBOARD_ROUTE);
-    } else {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
+
+  useEffect(() => {
+    if (!Cookies.get("LOGIN_TOKEN")) {
       router.push(routes.ADMIN_AUTH_LOGIN);
     }
   }, [router, token]);
 
   return (
-        <Suspense>
-          <AuthProvider>
-            {/* <ColorTheme>{children}</ColorTheme> */}
-            <AppColorThemeProvider>{children}</AppColorThemeProvider>
-          </AuthProvider>
-        </Suspense>
+    <Suspense>
+      <AuthProvider>
+        {/* <ColorTheme>{children}</ColorTheme> */}
+        <div className="dark:bg-boxdark-2 dark:text-bodydark">
+          {loading ? <Loader /> : children}
+        </div>
+        <AppColorThemeProvider>{children}</AppColorThemeProvider>
+      </AuthProvider>
+    </Suspense>
   );
 };
 export default AdminRootLayout;
